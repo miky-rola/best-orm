c2fec128d941fecf11931528d23e56420ccfee7e
miky-rola <mikyrola8@gmail.com>
2025-01-26 22:28:31 +0000

    removed the hashmap

--- src/models/base_model.rs
+++ src/models/base_model.rs
@@ -1,15 +1,33 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 use sea_query::Iden;
 use sqlx::{FromRow, postgres::PgRow};
 use crate::database::connection::{DatabaseConnection, OrmError};
 use crate::database::query_builder::QueryBuilder;
 
+pub enum Condition {
+    Equals(String, String),          
+    NotEquals(String, String),       
+    GreaterThan(String, String),    
+    GreaterThanOrEqual(String, String), 
+    LessThan(String, String),        
+    LessThanOrEqual(String, String), 
+    Like(String, String),            
+    NotLike(String, String),         
+    In(String, Vec<String>),        
+    NotIn(String, Vec<String>),      
+    IsNull(String),                  
+    IsNotNull(String),               
+    Between(String, String, String), 
+    NotBetween(String, String, String), 
+    And(Vec<Condition>),             
+    Or(Vec<Condition>),             
+}
 
 pub trait Model: Sized + Send + Sync + for<'r> FromRow<'r, PgRow> {
     type Columns: Iden;
     
-    fn find_by(db: &DatabaseConnection, conditions: &HashMap<String, String>) -> Result<Vec<Self>, OrmError>;
-    fn find_one_by(db: &DatabaseConnection, conditions: &HashMap<String, String>) -> Result<Option<Self>, OrmError>;
+    fn find_by(db: &DatabaseConnection, conditions: Vec<Condition>) -> Result<Vec<Self>, OrmError>;
+    fn find_one_by(db: &DatabaseConnection, conditions: Vec<Condition>) -> Result<Option<Self>, OrmError>;
     
     fn create(self, db: &DatabaseConnection) -> Result<Self, OrmError>;
     fn create_many(models: Vec<Self>, db: &DatabaseConnection) -> Result<Vec<Self>, OrmError>;
