9d833dc03bc9bebca550fab05fb397a6cb2675fa
miky-rola <mikyrola8@gmail.com>
2025-01-26 22:03:30 +0000

    removed the async

--- src/models/base_model.rs
+++ src/models/base_model.rs
@@ -1,25 +1,24 @@
 use std::collections::HashMap;
-use async_trait::async_trait;
 use sea_query::Iden;
 use sqlx::{FromRow, postgres::PgRow};
 use crate::database::connection::{DatabaseConnection, OrmError};
 use crate::database::query_builder::QueryBuilder;
 
-#[async_trait]
+
 pub trait Model: Sized + Send + Sync + for<'r> FromRow<'r, PgRow> {
     type Columns: Iden;
     
-    async fn find_by(db: &DatabaseConnection, conditions: &HashMap<String, String>) -> Result<Vec<Self>, OrmError>;
-    async fn find_one_by(db: &DatabaseConnection, conditions: &HashMap<String, String>) -> Result<Option<Self>, OrmError>;
+    fn find_by(db: &DatabaseConnection, conditions: &HashMap<String, String>) -> Result<Vec<Self>, OrmError>;
+    fn find_one_by(db: &DatabaseConnection, conditions: &HashMap<String, String>) -> Result<Option<Self>, OrmError>;
     
-    async fn create(self, db: &DatabaseConnection) -> Result<Self, OrmError>;
-    async fn create_many(models: Vec<Self>, db: &DatabaseConnection) -> Result<Vec<Self>, OrmError>;
+    fn create(self, db: &DatabaseConnection) -> Result<Self, OrmError>;
+    fn create_many(models: Vec<Self>, db: &DatabaseConnection) -> Result<Vec<Self>, OrmError>;
     
-    async fn update(&mut self, db: &DatabaseConnection) -> Result<(), OrmError>;
-    async fn update_by_id(&mut self, db: &DatabaseConnection, id: &str) -> Result<(), OrmError>;
+    fn update(&mut self, db: &DatabaseConnection) -> Result<(), OrmError>;
+    fn update_by_id(&mut self, db: &DatabaseConnection, id: &str) -> Result<(), OrmError>;
     
-    async fn delete(&self, db: &DatabaseConnection) -> Result<(), OrmError>;
-    async fn delete_by_id(db: &DatabaseConnection, id: &str) -> Result<(), OrmError>;
+    fn delete(&self, db: &DatabaseConnection) -> Result<(), OrmError>;
+    fn delete_by_id(db: &DatabaseConnection, id: &str) -> Result<(), OrmError>;
     
     fn query(db: &DatabaseConnection) -> QueryBuilder<Self>;
     
